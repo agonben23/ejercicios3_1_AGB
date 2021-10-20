90c3e33d43435dec3218679fd46bcb905ab2a724
agonben23 <agonzalezb@iesrafaelalberti.es>
2021-10-20 23:39:38 +0200

    ejercicio5_8 hecho

--- src/main/kotlin/ejercicio5_8.kt
+++ src/main/kotlin/ejercicio5_8.kt
@@ -1,16 +1,60 @@
-class Tiempo(var hora:Byte,var minuto:Byte?,var segundo: Byte?){
+class Tiempo(var hora: String, var minuto: String?, var segundo: String?) {
 
     override fun toString(): String {
         return ("La hora es $hora:$minuto:$segundo")
     }
 }
-fun main(){
+
+fun main() {
     println("Introduce la hora")
-    var hora= readLine()!!.toByte()
+    var hora = readLine().toString()
+    while (hora.isEmpty() || (hora.toByte() !in 0..23)) {
+        println("Hora no válida, vuelva a intentarlo")
+        println("Introduce la hora")
+        var hora = readLine()!!.toString()
+    }
     println("Introduce los minutos")
-    var minuto= readLine()?.toByte()
+    var minuto = readLine().toString()
+    if (minuto.isEmpty()) {
+        minuto = "00"
+    }
+    if (minuto.toByte() in 0..9) {
+        val cero = "0"
+        minuto = "$cero$minuto"
+    }
+    while (minuto.toByte() !in 0..59) {
+        println("Minuto no válido, vuelva a intentarlo")
+        println("Introduce los minutos")
+        var minuto = readLine().toString()
+        if (minuto.isEmpty()) {
+            minuto = "00"
+        }
+        if (minuto.toByte() in 0..9) {
+            val cero = "0"
+            minuto = "$cero$minuto"
+        }
+    }
     println("Introduce los segundos")
-    var segundo= readLine()?.toByte()
-    var Tiempo1 = Tiempo(hora,minuto,segundo)
+    var segundo = readLine().toString()
+    if (segundo.isEmpty()) {
+        segundo = "00"
+    }
+    if (segundo.toByte() in 0..9) {
+        val cero = "0"
+        segundo = "$cero$segundo"
+    }
+    while (segundo.toByte() !in 0..59) {
+        println("Segundo no válido, vuelva a intentarlo")
+        println("Introduce los segundos")
+        var segundo = readLine().toString()
+        if (segundo.isEmpty()) {
+            segundo = "00"
+        }
+        if (segundo.toByte() in 0..9) {
+            val cero = "0"
+            segundo = "$cero$segundo"
+        }
+    }
+    var Tiempo1 = Tiempo(hora, minuto, segundo)
     println(Tiempo1.toString())
 }
