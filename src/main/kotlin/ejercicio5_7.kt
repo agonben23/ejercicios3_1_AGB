66d7339ede7dbdaf1af7b4e5c67564d4d7481577
agonben23 <agonzalezb@iesrafaelalberti.es>
2021-10-20 21:43:27 +0200

    ejercicio5_8 a medio hacer

--- src/main/kotlin/ejercicio5_7.kt
+++ src/main/kotlin/ejercicio5_7.kt
@@ -1,12 +1,18 @@
-class Coche(var color: String,val marca:String,val modelo:String,val num_caballos: Int,val num_puertas: Byte,val matricula: String)
-{
-    fun cambiarColor(){
+class Coche(
+    var color: String,
+    val marca: String,
+    val modelo: String,
+    val num_caballos: Int,
+    val num_puertas: Byte,
+    val matricula: String
+) {
+    fun cambiarColor() {
         println("¿Cual es el nuevo color?")
-        color= readLine().toString()
-        while(color==""){
+        color = readLine().toString()
+        while (color == "") {
             println("Color no válido, vuelve a intentarlo")
             println("¿Cual es el nuevo color?")
-            color= readLine().toString()
+            color = readLine().toString()
         }
     }
 
@@ -15,8 +21,8 @@ class Coche(var color: String,val marca:String,val modelo:String,val num_caballo
     }
 }
 
-fun main(){
-    var c1 = Coche("blanco","Subaru","B360",240,4,"7665FNT")
+fun main() {
+    var c1 = Coche("blanco", "Subaru", "B360", 240, 4, "7665FNT")
     println(c1.toString())
     c1.cambiarColor()
     println(c1.toString())
