64355a45300e666051c1f7a1ca8b0c4ada7b28e2
agonben23 <agonzalezb@iesrafaelalberti.es>
2021-10-25 16:44:51 +0200

    ejercicio5_9 a medio hacer 2

--- src/main/kotlin/ejercicio5_9.kt
+++ src/main/kotlin/ejercicio5_9.kt
@@ -10,10 +10,17 @@ class Cuenta(val numCuenta: String, var saldo: Double) {
         val pago = readLine()!!.toDouble()
         saldo -= pago
     }
+
+    fun hacerTransferencia(importe : Double,cuentaIngreso : Cuenta){
+        saldo -= importe
+        cuentaIngreso.saldo =+ importe
+        println("Se ha realizado una transferencia de $importe desde la cuenta $numCuenta a la cuenta ${cuentaIngreso.numCuenta}")
+        println("El nuevo saldo de la cuenta de pago $numCuenta es $saldo")
+        println("El nuevo saldo de la cuenta de ingreso ${cuentaIngreso.numCuenta} es ${cuentaIngreso.saldo}")
+    }
 }
 
 class Persona(val dni: String) {
-    val listCuenta : List<Cuenta> = List<Cuenta>(3,)
     fun anadirCuenta() {
 
     }
@@ -26,16 +33,6 @@ class Persona(val dni: String) {
 fun main() {
     val cuenta1 = Cuenta("3453453GDG", 600.0)
     val cuenta2 = Cuenta("2323545DCA",450.0)
+    cuenta1.hacerTransferencia(20.0,cuenta2)
 
-    hacerTransferencia(cuenta1,cuenta2)
-
-}
-fun hacerTransferencia(cuentaPago : Cuenta,cuentaIngreso : Cuenta){
-    println("Introduzca el importe a transferir")
-    val impTransf = readLine()!!.toDouble()
-    cuentaPago.saldo -= impTransf
-    cuentaIngreso.saldo =+ impTransf
-    println("Se ha realizado una transferencia de $impTransf desde la cuenta ${cuentaPago.numCuenta} a la cuenta ${cuentaIngreso.numCuenta}")
-    println("El nuevo saldo de la cuenta de pago ${cuentaPago.numCuenta} es ${cuentaPago.saldo}")
-    println("El nuevo saldo de la cuenta de ingreso ${cuentaIngreso.numCuenta} es ${cuentaIngreso.saldo}")
 }
