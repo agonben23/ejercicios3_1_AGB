47499e508d9f15ff58138bd7d17f7f7bccc0ee9e
agonben23 <agonzalezb@iesrafaelalberti.es>
2021-10-26 01:41:05 +0200

    ejercicio5_9 a medio hacer 4

--- src/main/kotlin/ejercicio5_9.kt
+++ src/main/kotlin/ejercicio5_9.kt
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.TestOnly
+
 class Cuenta(val numCuenta: String, var saldo: Double) {
     fun recibirAbono() {
         println("Introduzca el saldo a añadir")
@@ -13,7 +15,7 @@ class Cuenta(val numCuenta: String, var saldo: Double) {
 
     fun hacerTransferencia(importe: Double, cuentaIngreso: Cuenta) {
         saldo -= importe
-        cuentaIngreso.saldo = +importe
+        cuentaIngreso.saldo += importe
         println("Se ha realizado una transferencia de $importe desde la cuenta $numCuenta a la cuenta ${cuentaIngreso.numCuenta}")
         println("El nuevo saldo de la cuenta de pago $numCuenta es $saldo")
         println("El nuevo saldo de la cuenta de ingreso ${cuentaIngreso.numCuenta} es ${cuentaIngreso.saldo}")
@@ -30,18 +32,28 @@ class Persona(val dni: String) {
     }
 
     fun esMoroso() {
-        val contadorMax = lisCuenta.size - 1
-        for (i in 0..contadorMax) {
-
-        }
+        var moroso : Boolean
+        val contadorMax = lisCuenta.size
+        var i = 0
+        do {
+            var cActual = lisCuenta.get(i)
+            moroso = cActual.saldo < 0
+            i += 1
+        }while (moroso || (i == contadorMax))
+        if (moroso == true){
+            println("Esta persona es morosa")
+        }else
+            println("Esta persona no es morosa")
     }
 }
 
 fun main() {
     val cuenta1 = Cuenta("3453453GDG", 600.0)
-    val cuenta2 = Cuenta("2323545DCA", 450.0)
+    val cuenta2 = Cuenta("2323545DCA",-150.0)
     val persona1 = Persona("4545745H")
     persona1.anadirCuenta(cuenta1)
+    persona1.anadirCuenta(cuenta2)
+    persona1.esMoroso()
     cuenta1.hacerTransferencia(20.0, cuenta2)
 
 }
