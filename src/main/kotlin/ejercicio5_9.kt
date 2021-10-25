b8a45e88b96ba7a7b18dad50acc22f52554becaa
agonben23 <agonzalezb@iesrafaelalberti.es>
2021-10-25 21:43:12 +0200

    ejercicio5_9 a medio hacer 3

--- src/main/kotlin/ejercicio5_9.kt
+++ src/main/kotlin/ejercicio5_9.kt
@@ -11,9 +11,9 @@ class Cuenta(val numCuenta: String, var saldo: Double) {
         saldo -= pago
     }
 
-    fun hacerTransferencia(importe : Double,cuentaIngreso : Cuenta){
+    fun hacerTransferencia(importe: Double, cuentaIngreso: Cuenta) {
         saldo -= importe
-        cuentaIngreso.saldo =+ importe
+        cuentaIngreso.saldo = +importe
         println("Se ha realizado una transferencia de $importe desde la cuenta $numCuenta a la cuenta ${cuentaIngreso.numCuenta}")
         println("El nuevo saldo de la cuenta de pago $numCuenta es $saldo")
         println("El nuevo saldo de la cuenta de ingreso ${cuentaIngreso.numCuenta} es ${cuentaIngreso.saldo}")
@@ -21,18 +21,27 @@ class Cuenta(val numCuenta: String, var saldo: Double) {
 }
 
 class Persona(val dni: String) {
-    fun anadirCuenta() {
-
+    val lisCuenta = arrayListOf<Cuenta>()
+    fun anadirCuenta(cuentaNueva: Cuenta) {
+        if (lisCuenta.size <= 3) {
+            lisCuenta.add(cuentaNueva)
+        } else
+            println("Esta persona ya tiene tres cuentas asociadas")
     }
 
     fun esMoroso() {
+        val contadorMax = lisCuenta.size - 1
+        for (i in 0..contadorMax) {
 
+        }
     }
 }
 
 fun main() {
     val cuenta1 = Cuenta("3453453GDG", 600.0)
-    val cuenta2 = Cuenta("2323545DCA",450.0)
-    cuenta1.hacerTransferencia(20.0,cuenta2)
+    val cuenta2 = Cuenta("2323545DCA", 450.0)
+    val persona1 = Persona("4545745H")
+    persona1.anadirCuenta(cuenta1)
+    cuenta1.hacerTransferencia(20.0, cuenta2)
 
 }
